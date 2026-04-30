ed6664d1597da830b6baa348f7ebc2b4c061656b
NicolasAlvSouza <123672066+NicolasAlvSouza@users.noreply.github.com>
2026-04-30 23:01:50 +0000

    Delete

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,3 +1,40 @@
-const listar = (req, res) => {res.json([])}
+const usuarios = [
+  {
+    id: Date.now(),
+    nome: "João Silva",
+    telefone: "27 99999-1111",
+    email: "joao.silva@email.com",
+    idade: 28
+  },
+  {
+    nome: "Maria Oliveira",
+    telefone: "27 98888-2222",
+    email: "maria.oliveira@email.com",
+    idade: 34
+  },
+  {
+    id: Date.now(),
+    nome: "Carlos Souza",
+    telefone: "27 97777-3333",
+    email: "carlos.souza@email.com",
+    idade: 22
+  },
+  {
+    id: Date.now(),
+    nome: "Ana Costa",
+    telefone: "27 96666-4444",
+    email: "ana.costa@email.com",
+    idade: 30
+  },
+  {
+    id: Date.now(),
+    nome: "Nicolas alves",
+    telefone: "27 99903-4458",
+    email: "nicolas.alves@email.com",
+    idade: 21
+  }
+]
+
+const listar = (req, res) => {res.json([usuarios])}
 
 export default { listar }
